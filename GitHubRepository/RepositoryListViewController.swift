dab417c73c25ce46ecc44872eb30f34148c33c6c
최지연 <choi02647@naver.com>
2023-05-30 12:25:48 +0900

    added fetchRepositories

--- GitHubRepository/RepositoryListViewController.swift
+++ GitHubRepository/RepositoryListViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RepositoryListViewController: UITableViewController {
     
     private let organization = "Apple"
-   
+    private let repositories = BehaviorSubject<[Repository]>(value: [])
+    private let bag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = organization + " Repositories"
@@ -27,6 +31,56 @@ class RepositoryListViewController: UITableViewController {
     }
     
     @objc func refresh() {}
+    
+    func fetchRepositories(of organization: String) {
+        Observable.from([organization])
+            .map { organization -> URL in
+                return URL(string: "https://api.github.com/orgs/\(organization)/repos")!
+            }
+            .map { url -> URLRequest in
+                var request = URLRequest(url: url)
+                request.httpMethod = "GET"
+                return request
+            }
+            .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+                return URLSession.shared.rx.response(request: request)
+            }
+            .filter { response, _ in
+                return 200..<300 ~= response.statusCode
+            }
+            .map { _, data -> [[String: Any]] in
+                guard let json = try? JSONSerialization.jsonObject(with: data, options: []),
+                      let result = json as? [[String: Any]] else {
+                    return []
+                }
+                return result
+            }
+            .filter { objects in
+                return objects.count > 0
+            }
+            .map { objects in
+                return objects.compactMap { dic -> Repository? in
+                    guard let id = dic["id"] as? Int,
+                          let name = dic["name"] as? String,
+                          let description = dic["description"] as? String,
+                          let stargazersCount = dic["stargazers_count"] as? Int,
+                          let language = dic["language"] as? String else {
+                        return nil
+                    }
+                    
+                    return Repository(id: id, name: name, description: description, stargazersCount: stargazersCount, language: language)
+                }
+            }
+            .subscribe(onNext: {[weak self] newRepositories in
+                self?.repositories.onNext(newRepositories)
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                    self?.refreshControl?.endRefreshing()
+                }
+            })
+            .disposed(by: bag)
+    }
 }
 
 extension RepositoryListViewController {

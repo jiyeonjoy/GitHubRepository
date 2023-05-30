b1a9582fab13bf7b967e757ced0a207977f3f531
최지연 <choi02647@naver.com>
2023-05-30 12:12:19 +0900

    added cell ui , Repository

---
+++ GitHubRepository/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  GitHubRepository
+//
+//  Created by 최지연/클라이언트 on 2023/05/30.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
